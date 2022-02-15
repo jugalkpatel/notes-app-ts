e9e53e1f46d8e6d7e5ec5be7488443e8c88ecbab
jimmy <ptjimmy99@gmail.com>
2022-02-15 21:15:12 +0530

    add catch-all route

---
+++ pages/notes/[...params].tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+
+// if we have params and dynamic route under same index page
+// then params route take precedence over catch-all for one param ----> if url is /notes/1/ ----> then [id].jsx render
+// ----> if url is /notes/1/2/3 ----> then [...params].jsx render
+
+const CatchAll: NextPage = () => {
+  const router = useRouter();
+  const params = router.query.params || [];
+  console.log({ params });
+  return <div>notes catch all route</div>;
+};
+
+export default CatchAll;
