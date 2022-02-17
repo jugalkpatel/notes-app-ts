1b776d4ccc856f356de13bff73f6626c7fa46f59
jimmy <ptjimmy99@gmail.com>
2022-02-17 22:48:13 +0530

    add getStaticProps function

--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -1,7 +1,14 @@
-import type { NextPage } from 'next';
+import type { NextPage, NextPageContext } from "next";
 
 const Notes: NextPage = () => {
   return <div>notes index route</div>;
 };
+
+export async function getStaticProps(context: NextPageContext) {
+  console.log({ context });
+  return {
+    props: {},
+  };
+}
 
 export default Notes;
