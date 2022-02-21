4e478504d25cbcbcbb58c43483eb89eba1d6c17d
jimmy <ptjimmy99@gmail.com>
2022-02-21 19:54:11 +0530

    use getStaticProps in root page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App({ Component, pageProps }) {
-  console.log(process.env.SECRET);
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage, InferGetStaticPropsType } from "next";
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+
+  //add check for empty response
+
+  const data = await response.json();
+
+  return { props: { data } };
+};
+
+const Home: NextPage = ({
+  data,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <div>root</div>
-  )
-}
+    <div>
+      <pre>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
